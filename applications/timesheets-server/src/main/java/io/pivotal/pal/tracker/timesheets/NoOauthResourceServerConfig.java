376be9cf2e9d3f0521645722270067578d1bf876
PAL-8 <suresh.kpc@gmail.com>
2020-03-03 17:04:54 +0530

    new changes

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/NoOauthResourceServerConfig.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/NoOauthResourceServerConfig.java
@@ -1,13 +1,9 @@
 package io.pivotal.pal.tracker.timesheets;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @ConditionalOnProperty(value = "application.oauth-enabled", havingValue = "false")
@@ -17,11 +13,4 @@ public class NoOauthResourceServerConfig extends ResourceServerConfigurerAdapter
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
     }
-
-    @Bean
-    @LoadBalanced
-    public RestOperations restOperations() {
-        return new RestTemplate();
-    }
 }
-
