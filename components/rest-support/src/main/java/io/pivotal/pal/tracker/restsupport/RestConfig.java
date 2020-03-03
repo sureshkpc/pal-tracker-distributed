4cb07bebba80d4fb4a74ced81f4bae0c6b67296c
PAL-8 <suresh.kpc@gmail.com>
2020-03-03 15:40:52 +0530

    Spring security OATH Changes

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -12,12 +12,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
 
-    @LoadBalanced
+ /*   @LoadBalanced
     @Bean
     public RestOperations restOperations() {
         return new RestTemplate();
     }
-
+*/
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
